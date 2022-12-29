b344fcef735b2885c512b6e46caee35003d11a00
KateM3d <ekaterina8medvedeva@gmail.com>
2022-12-29 17:20:06 -0500

    add solution2

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,34 @@
+import java.util.Arrays;
+
 public class Main {
-    private static String glueInput(String input) {
-        StringBuilder sb = new StringBuilder();
+    static char[] letters = {'a', 'b', 'C', '*', '^', '!', 'd', '!'};
+    static char[] reversed = new char[letters.length];
 
-        for (char c : input.toCharArray()) {
-            if (c != ' ') {
-                sb.append(c);
+    static boolean checkIfEmpty(int position) {
+        return reversed[position] == '\0';
+    }
+
+    static void reverse(char[] array) {
+        int j = 0;
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] >= 'a' && array[i] <= 'z' || array[i] >= 'A' && array[i] <= 'Z') {
+                while (!checkIfEmpty(j)) {
+                    j++;
+                    checkIfEmpty(j);
+                }
+                reversed[j] = letters[i];
+                j++;
+            } else {
+                int jCurrent = j;
+                j = i;
+                reversed[j] = array[i];
+                j = jCurrent;
             }
+            System.out.println(Arrays.toString(reversed));
         }
-
-        return sb.toString();
-    }
-
-    private static String[] bisectInput(String input) {
-        StringBuilder word1 = new StringBuilder();
-        StringBuilder word2 = new StringBuilder();
-
-        for (int i = 0, j = (input.length()) / 2; i <= input.length() / 2 && j < input.length(); i++, j++) {
-            word1.append(input.charAt(i));
-            word2.append(input.charAt(j));
-        }
-
-        String[] bisectInputResult = new String[]{word1.toString(), " ", word2.toString()};
-        return bisectInputResult;
-    }
-
-    private static String reverse(String word) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = word.length() - 1; i >= 0; i--) {
-            sb.append(word.charAt(i));
-        }
-        return sb.toString();
-    }
-
-    private static String anagramize(String input) {
-        StringBuilder sb = new StringBuilder();
-
-        for (String s : bisectInput(glueInput(input))) {
-            sb.append(reverse(s));
-        }
-        return sb.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println("abcd efgh" + " => " + anagramize("abcd efgh")); //"dcba hgfe"
-        System.out.println("a 1 bcdefg!h" + " => " + anagramize("a 1 bcdefg!h")); //"d1cba hgf!e"
+        reverse(letters);
     }
-
 }
