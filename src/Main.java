79530b2f76d2795252083858f152e21fe3c57171
KateM3d <ekaterina8medvedeva@gmail.com>
2023-01-02 17:14:12 -0500

    final solution

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,36 @@
 public class Main {
-    private static String glueInput(String input) {
-        StringBuilder sb = new StringBuilder();
-
-        for (char c : input.toCharArray()) {
-            if (c != ' ') {
-                sb.append(c);
+    static char[] reverse(char[] array) {
+        char[] reversed = new char[array.length];
+        int j = 0;
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] >= 'a' && array[i] <= 'z' || array[i] >= 'A' && array[i] <= 'Z') {
+                while (reversed[j] != '\0') {
+                    j++;
+                }
+                reversed[j] = array[i];
+                j++;
+            } else {
+                int jCurrent = j;
+                j = i;
+                reversed[j] = array[i];
+                j = jCurrent;
             }
         }
-
-        return sb.toString();
+        return reversed;
     }
 
-    private static String[] bisectInput(String input) {
-        StringBuilder word1 = new StringBuilder();
-        StringBuilder word2 = new StringBuilder();
-
-        for (int i = 0, j = (input.length()) / 2; i <= input.length() / 2 && j < input.length(); i++, j++) {
-            word1.append(input.charAt(i));
-            word2.append(input.charAt(j));
+    public static String toAnagram(String input) {
+        String[] words = input.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (String w : words) {
+            char[] wordReversed = reverse(w.toCharArray());
+            result.append(new String(wordReversed));
+            result.append(" ");
         }
-
-        String[] bisectInputResult = new String[]{word1.toString(), " ", word2.toString()};
-        return bisectInputResult;
-    }
-
-    private static String reverse(String word) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = word.length() - 1; i >= 0; i--) {
-            sb.append(word.charAt(i));
-        }
-        return sb.toString();
-    }
-
-    private static String anagramize(String input) {
-        StringBuilder sb = new StringBuilder();
-
-        for (String s : bisectInput(glueInput(input))) {
-            sb.append(reverse(s));
-        }
-        return sb.toString();
+        return result.toString().trim();
     }
 
     public static void main(String[] args) {
-        System.out.println("abcd efgh" + " => " + anagramize("abcd efgh")); //"dcba hgfe"
-        System.out.println("a 1 bcdefg!h" + " => " + anagramize("a 1 bcdefg!h")); //"d1cba hgf!e"
+        System.out.println(toAnagram("a 1 bcdefg!h"));
     }
-
 }
